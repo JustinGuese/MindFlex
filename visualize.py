a0462ef4b68b958ed7afc6a7f698aef9a2476d67
Justin Güse <guese.justin@gmail.com>
2015-10-10 02:47:45 +0200

    update in one window

--- visualize.py
+++ visualize.py
@@ -2,18 +2,16 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-#signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma
+names = ["signal strength", "attention", "meditation", "delta", "theta", "low alpha", "high alpha", "low beta", "high beta", "low gamma", "high gamma"]
 plt.ion()
-fig = plt.figure()
 
 signals = [[],[],[],[],[],[],[],[],[],[],[]]
 nrs = numpy.arange(len(signals))
 
-
 def gotMessage(array):
 	for i in nrs:
-		print i
 		signals[i].append(array[i])
-		ax = fig.add_subplot(4,4,i)
-		ax.plot(signals[i])
-		plt.draw()
+	plt.cla()
+	plt.plot(array,'*')
+	#plt.set_xticklabels(names)
+	plt.draw()
