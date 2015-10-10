b0a675d238fe68fca15422f67c64f6ac1d51f81c
Justin Güse <guese.justin@gmail.com>
2015-10-10 02:25:04 +0200

    working but subplots

--- visualize.py
+++ visualize.py
@@ -1,9 +1,19 @@
 import numpy
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma
+plt.ion()
+fig = plt.figure()
+
+signals = [[],[],[],[],[],[],[],[],[],[],[]]
+nrs = numpy.arange(len(signals))
+
 
 def gotMessage(array):
-	print "I got:"
-	for elem in array:
-		print elem
-
-
+	for i in nrs:
+		print i
+		signals[i].append(array[i])
+		ax = fig.add_subplot(4,4,i)
+		ax.plot(signals[i])
+		plt.draw()
